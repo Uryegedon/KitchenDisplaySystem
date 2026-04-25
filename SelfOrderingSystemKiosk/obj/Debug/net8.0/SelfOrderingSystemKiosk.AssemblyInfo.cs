08f45e7d83ed4ecc72d4418e5ea0b95877faaa8a
Uryegedon <uryegedon@gmail.com>
2026-04-26 00:53:10 +0800

    more changes

--- SelfOrderingSystemKiosk/obj/Debug/net8.0/SelfOrderingSystemKiosk.AssemblyInfo.cs
+++ SelfOrderingSystemKiosk/obj/Debug/net8.0/SelfOrderingSystemKiosk.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SelfOrderingSystemKiosk")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad7c8676a099d0eb2d273838897ca4846f4956ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb610b5742503acd295d6e444bc02767ca275710")]
 [assembly: System.Reflection.AssemblyProductAttribute("SelfOrderingSystemKiosk")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SelfOrderingSystemKiosk")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
